5fff37e74982d44f7fb78be668ec941b8a37d41f
eungwang <ekr2677@naver.com>
2024-10-10 11:11:12 +0900

    git commit -m "2024.10.10 - 10"

--- app/src/main/java/com/example/myapplication/BoardActivity.java
+++ app/src/main/java/com/example/myapplication/BoardActivity.java
@@ -1,7 +1,12 @@
 package com.example.myapplication;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class BoardActivity extends AppCompatActivity{
 
@@ -9,6 +14,23 @@ public class BoardActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        // 리스너 추가하여 메뉴 선택 시 동작 정의
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.nav_write) {
+                    Intent intent = new Intent(BoardActivity.this,BoardwriteActivity.class);
+                    startActivity(intent);
+                    // 작성 아이템 클릭 시 처리
+                    return true;
+                } else
+                return false;
+            }
+        });
 
     }
 }
