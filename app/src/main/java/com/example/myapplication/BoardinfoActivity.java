ab7d660933c350ab88f40b19b6c3abc39f76c89d
LSH <dldn99022@naver.com>
2024-10-10 10:19:28 +0900

    git commit -m "이상헌 - 1"

--- app/src/main/java/com/example/myapplication/BoardinfoActivity.java
+++ app/src/main/java/com/example/myapplication/BoardinfoActivity.java
@@ -1,7 +1,9 @@
 package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.ImageView;
 
 public class BoardinfoActivity extends AppCompatActivity{
 
@@ -10,5 +12,14 @@ public class BoardinfoActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board_info);
 
+        ImageView backButton = findViewById(R.id.iv_arrow_left_board); // < 버튼의 id를 backButton으로 설정
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 새로운 Activity로 이동
+                Intent intent = new Intent(BoardinfoActivity.this, BoardActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
