172ff49d5a77412505aa9f60059c9f2ccabca51d
eungwang <ekr2677@naver.com>
2024-10-10 12:30:00 +0900

    git commit -m "2024.10.10 - 12"

--- app/src/main/java/com/example/myapplication/BoardwriteActivity.java
+++ app/src/main/java/com/example/myapplication/BoardwriteActivity.java
@@ -1,7 +1,14 @@
 package com.example.myapplication;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class BoardwriteActivity extends AppCompatActivity{
 
@@ -9,6 +16,18 @@ public class BoardwriteActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board_write);
+
+        ImageView Backview = findViewById(R.id.iv_arrow_left_board);
+
+        Backview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 클릭 시 실행할 작업을 여기에 작성
+
+                // 예: 뒤로 가기 동작 구현
+                finish();  // 현재 액티비티를 종료하고 이전 액티비티로 돌아갑니다.
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/myapplication/ProfileActivity.java
+++ app/src/main/java/com/example/myapplication/ProfileActivity.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +12,18 @@ public class ProfileActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.actiivty_profile);
+
+        ImageView Backview = findViewById(R.id.iv_arrow_left_board);
+
+        Backview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 클릭 시 실행할 작업을 여기에 작성
+
+                // 예: 뒤로 가기 동작 구현
+                finish();  // 현재 액티비티를 종료하고 이전 액티비티로 돌아갑니다.
+            }
+        });
 
     }
 }
