53ce97d8968948b28e8936a1bf0c9073e07b3adc
eungwang <ekr2677@naver.com>
2024-10-11 10:38:29 +0900

    git commit -m "2024.10.11 - 1"

--- app/src/main/java/com/example/myapplication/ResultActivity.java
+++ app/src/main/java/com/example/myapplication/ResultActivity.java
@@ -4,16 +4,19 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -50,6 +53,26 @@ public class ResultActivity extends AppCompatActivity {
 
                 Intent intent = new Intent(ResultActivity.this,InfoActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        // BottomNavigationView 설정
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.nav_board) {
+                    startActivity(new Intent(ResultActivity.this, BoardActivity.class));
+                    return true;
+                } else if (id == R.id.nav_home) {
+                    startActivity(new Intent(ResultActivity.this, MainActivity.class));
+                    return true;
+                } else if (id == R.id.nav_profile) {
+                    startActivity(new Intent(ResultActivity.this, ProfileActivity.class));
+                    return true;
+                }
+                return false;
             }
         });
     }
