7ecad8d1ba442d68f6472138df519dbb05d4abd1
qwe213 <khw123456789p@gmailcom>
2024-10-11 12:48:18 +0900

    git commit -m "강현우"

--- app/src/main/java/com/example/myapplication/InfoActivity.java
+++ app/src/main/java/com/example/myapplication/InfoActivity.java
@@ -16,7 +16,7 @@ public class InfoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_info);
+        setContentView(R.layout.activity_result);
 
         Button Okaybutton = findViewById(R.id.confirm_button);
 
