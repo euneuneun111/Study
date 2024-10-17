e640b77e9992bb3860dd2757ddc77d7287eb0a70
ga352 <jeongautumn02@gmail.com>
2024-10-17 15:03:41 +0900

    git commit -m "정가을"

---
+++ app/src/main/java/com/example/myapplication/Post.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+public class Post {
+    private String p_title;
+    private String p_content;
+    private String p_img;
+
+    public String getP_title() {
+        return p_title;
+    }
+
+    public String getP_content() {
+        return p_content;
+    }
+
+    public String getP_img() {
+        return p_img;
+    }
+}
+
