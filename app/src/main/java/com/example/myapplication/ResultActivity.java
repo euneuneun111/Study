484e01a9f856624d249e7a4051a648148cfb9a5a
eungwang <ekr2677@naver.com>
2024-10-21 10:28:06 +0900

    git commit -m "2024.10.21 - 1"

--- app/src/main/java/com/example/myapplication/ResultActivity.java
+++ app/src/main/java/com/example/myapplication/ResultActivity.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+/*package com.example.myapplication;
 
 import android.content.Intent;
 import android.content.res.AssetFileDescriptor;
@@ -273,4 +273,4 @@ public class ResultActivity extends AppCompatActivity {
             return probability;
         }
     }
-}
+}*/
