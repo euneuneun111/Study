c2c2cda8e0228b898df08c4eac7c0127160beb7d
euneuneun111 <softeuneun1@gmail.com>
2025-06-18 13:11:36 +0900

    Update Method02.java

--- method/Method02.java
+++ method/Method02.java
@@ -1,14 +1,18 @@
 package method;
 
 public class Method02 {
-	   public static int div( ) {        
-	      int a = 10, b = 5;
-	      int result = a / b;
-	      
-	      return result;
-	   } 
-	   public static void main(String[] args) {   
-	     int num = div(); 
-	     System.out.println(num);   
-	   }
-	}
+    
+    // 매개변수 없이 두 수를 나누고 결과를 반환하는 메서드
+    public static int div() {
+        int a = 10;
+        int b = 5;
+        int result = a / b;  // 10 나누기 5
+        
+        return result;       // 결과 반환 (2)
+    }
+    
+    public static void main(String[] args) {
+        int num = div();     // div 메서드 호출 후 결과를 num에 저장
+        System.out.println(num);  // 결과 출력: 2
+    }
+}
