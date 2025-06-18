eafab66848e788c4d87552d2f3117e61546771de
euneuneun111 <softeuneun1@gmail.com>
2025-06-18 13:08:02 +0900

    Update Ex01.java

--- constructor/Ex01.java
+++ constructor/Ex01.java
@@ -2,20 +2,28 @@ package constructor;
 
 public class Ex01 {
 
-	int a,b,c;
-	
-	public Ex01() {};
-	public Ex01(int a) {
-		this(a,0,0);
-	}
-	public Ex01(int a, int b) {
-		this(a,b,0);
-	}
-	public Ex01(int a, int b, int c) {
-		super();
-		this.a = a;
-		this.b = b;
-		this.c = c;
-	}
-	
+    // 멤버 변수 3개 선언
+    int a, b, c;
+
+    // 기본 생성자: 아무 값도 초기화하지 않음
+    public Ex01() {}
+
+    // 매개변수 1개 생성자: a만 초기화, b와 c는 0으로
+    public Ex01(int a) {
+        this(a, 0, 0);  // 다른 생성자 호출
+    }
+
+    // 매개변수 2개 생성자: a, b 초기화, c는 0으로
+    public Ex01(int a, int b) {
+        this(a, b, 0);  // 다른 생성자 호출
+    }
+
+    // 매개변수 3개 생성자: a, b, c 모두 초기화
+    public Ex01(int a, int b, int c) {
+        super();        // 부모 생성자 호출 (Object 클래스)
+        this.a = a;     // 멤버 변수에 값 대입
+        this.b = b;
+        this.c = c;
+    }
 }
+
