ca6f35066da3baba52a227bc65a40ddbd873ffef
euneuneun111 <softeuneun1@gmail.com>
2025-06-18 11:35:56 +0900

    Add files via upload

---
+++ classes/Person.java
@@ -0,0 +1,14 @@
+package classes;
+
+public class Person {
+	
+	String name;	
+	int reg_num;	
+	
+	void walk() {
+	}
+	String run() {
+		return "run";
+	}
+
+}
---
+++ constructor/Ex01.java
@@ -0,0 +1,21 @@
+package constructor;
+
+public class Ex01 {
+
+	int a,b,c;
+	
+	public Ex01() {};
+	public Ex01(int a) {
+		this(a,0,0);
+	}
+	public Ex01(int a, int b) {
+		this(a,b,0);
+	}
+	public Ex01(int a, int b, int c) {
+		super();
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+	
+}
