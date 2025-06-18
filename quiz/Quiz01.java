b88f5e0ef364afc770d1f134b6f30ce894d824ec
euneuneun111 <softeuneun1@gmail.com>
2025-06-18 13:15:51 +0900

    Update Quiz01.java

--- quiz/Quiz01.java
+++ quiz/Quiz01.java
@@ -2,15 +2,16 @@ package quiz;
 
 public class Quiz01 {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		for (int j = 0; j < 8; j++) {
-			for (int i = 0; i < j + 1; i++) {
-				System.out.print('*');
-			}
+        // 8줄 반복
+        for (int j = 0; j < 8; j++) {
+            // 각 줄마다 j+1개의 별 출력
+            for (int i = 0; i < j + 1; i++) {
+                System.out.print('*');  // 별 출력 (줄 바꿈 없이)
+            }
 
-			System.out.println();
-
-		}
-	}
+            System.out.println(); // 한 줄 끝나면 줄 바꿈
+        }
+    }
 }
