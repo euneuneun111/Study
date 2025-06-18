32bf59415281e2889d892719ebcf4f0b9208bbfa
euneuneun111 <softeuneun1@gmail.com>
2025-06-18 11:35:56 +0900

    Add files via upload

---
+++ array/Array_String.java
@@ -0,0 +1,12 @@
+package array;
+
+public class Array_String {
+
+	public static void main(String[] args) {
+		String[] strArr = new String[5];
+		
+		
+
+	}
+
+}
---
+++ test/Exam01.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Exam01 {
+	public static void main(String[] args) {
+		final int SPEED = 20;
+		int count = 10;
+		// SPEED = 40;
+		count = 11;
+		System.out.print("speed");
+		System.out.println(SPEED);
+		System.out.print("count");
+		System.out.println(count);
+	}
+}
