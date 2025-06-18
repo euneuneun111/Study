e758699b61a61bf3a5180ae8fc656c213bb81c60
euneuneun111 <softeuneun1@gmail.com>
2025-06-18 13:04:47 +0900

    Update Person.java

--- classes/Person.java
+++ classes/Person.java
@@ -1,14 +1,20 @@
 package classes;
 
 public class Person {
-	
-	String name;	
-	int reg_num;	
-	
-	void walk() {
-	}
-	String run() {
-		return "run";
-	}
-
+    
+    // 사람 이름
+    String name;
+    
+    // 등록 번호 (예: 주민등록번호)
+    int reg_num;
+    
+    // 걷기 동작을 수행하는 메서드 (아직 내용 없음)
+    void walk() {
+        // 걷는 행동 구현 예정
+    }
+    
+    // 달리기 동작을 수행하고 "run" 문자열을 반환하는 메서드
+    String run() {
+        return "run";
+    }
 }
